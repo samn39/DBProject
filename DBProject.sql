7c2cca89ae42c56285c3f350e8394d37e97f122f
Sanviseth <91292916+39samn@users.noreply.github.com>
2022-04-07 12:28:22 -0400

    initial sql file
    make schema out of this plz!

---
+++ DBProject.sql
@@ -0,0 +1,5 @@
+Create table account (account_ID varChar(10), password varchar(50), primary key(account_ID));
+
+insert into account values("bob123", "hi123"), ("cait123", "cs123"), ("samn123", "idk123");
+
+Select * from account;
