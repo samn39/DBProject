e4d9fdd537161449c6df2dfc53ed7227f5cf2bcd
Sanviseth <91292916+39samn@users.noreply.github.com>
2022-04-08 21:32:04 -0400

    submission folder

--- DBProject.sql
+++
@@ -1,5 +0,0 @@
-Create table account (username varChar(10), password varchar(50), primary key(account_ID));
-
-insert into account values("bob123", "hi123"), ("cait123", "cs123"), ("samn123", "idk123");
-
-Select * from account;
